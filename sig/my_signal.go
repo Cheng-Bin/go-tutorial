6e402cec3b699bcb6eda120149fda19296187d94
ChengBin <9971556658@qq.com>
2016-12-15 17:18:23 +0800

    add my_signal.go

--- sig/my_signal.go
+++ sig/my_signal.go
@@ -1,22 +1,78 @@
-package signal
+package main
 
 import (
-	"os/exec"
-	"fmt"
-	"strconv"
-	"os"
-	"syscall"
-	"bytes"
-	"io"
+    "bytes"
+    "errors"
+    "fmt"
+    "io"
+    "os"
+    "os/exec"
+    "os/signal"
+    "strconv"
+    "strings"
+    "sync"
+    "syscall"
+    "time"
 )
 
-func MySignal() {
+func main() {
+
+    go func() {
+        time.Sleep(5 * time.Second)
+        sigSendingDemo()
+    }()
+
+    sigHandleDemo()
+}
+
+func sigHandleDemo() {
+
+    sigRecv1 := make(chan os.Signal, 1)
+    sigs1 := []os.Signal{syscall.SIGINT, syscall.SIGQUIT}
+    fmt.Printf("Set notification for %s... [sigRecv1]\n", sigs1)
+    signal.Notify(sigRecv1, sigs1...)
+
+    sigRecv2 := make(chan os.Signal, 1)
+    sigs2 := []os.Signal{syscall.SIGQUIT}
+    fmt.Printf("Set notification for %s... [sigRecv2]\n", sigs2)
+    signal.Notify(sigRecv2, sigs2...)
+
+    var wg sync.WaitGroup
+    wg.Add(2)
+
+    go func() {
+        for sig := range sigRecv1 {
+            fmt.Printf("Received a signal from sigRecv1: %s\n", sig)
+        }
+        fmt.Printf("End. [sigRecv1]\n")
+        wg.Done()
+    }()
+
+    go func() {
+        for sig := range sigRecv2 {
+            fmt.Printf("Received a signal from sigRecv2: %s\n", sig)
+        }
+        fmt.Printf("End. [sigRecv2]\n")
+        wg.Done()
+    }()
+
+    fmt.Printf("Wait for 2 seconds ... ")
+    time.Sleep(2 * time.Second)
+    fmt.Printf("stop notification...")
+    signal.Stop(sigRecv1)
+    close(sigRecv1)
+    fmt.Printf("done. [sigRecv1]\n")
+    wg.Wait()
+
+}
+
+func sigSendingDemo() {
 
     cmds := []*exec.Cmd{
         exec.Command("ps", "aux"),
-        exec.Command("grep", "mysignal.go"),
+        exec.Command("grep", "my_signal.go"),
         exec.Command("grep", "-v", "grep"),
-        exec.Command("awk", "{print $2}")
+        exec.Command("awk", "{print $2}"),
     }
 
     output, err := runCmds(cmds)
@@ -25,49 +81,125 @@ func MySignal() {
         return
     }
 
-    pid := strconv.Atoi(output)
-    proc, err := os.FindProcess(pid)
+    pids, err := getPids(output)
 
-    err = proc.Signal(syscall.SIGINT)
+    fmt.Printf("Target PID(s): \n%v\n", pids)
 
+    for _, pid := range pids {
+        proc, err := os.FindProcess(pid)
+        if err != nil {
+            fmt.Printf("Process Finding Error: %s\n", err)
+            return
+        }
 
+        sig := syscall.SIGQUIT
+        fmt.Printf("Send signal '%s' to the process (pid = %d) ...\n", sig, pid)
+        err = proc.Signal(sig)
+        if err != nil {
+            fmt.Printf("Signal Sending Error: %s \n", err)
+            return
+        }
+    }
 
 }
 
-
 func runCmds(cmds []*exec.Cmd) ([]string, error) {
 
-    var outputbuffer bytes.Buffer
-    var stdout io.ReadCloser
-    var stdin io.WriteCloser
-
-    for _, cmd := range cmds {
-
-        if stdout != nil {
-            outputbuffer := bufio.NewReader(stdout)
-            stdin, err := cmd.StdinPipe()
-            if err != nil {
-
-            }
-            outputbuffer.WriteTo(stdin)
-        }
-
-        stdout, err := cmd.StdoutPipe()
-        if err != nil {
-            Failed(err, "Get StdoutPipe failed.")
-        }
-
-        if err := cmd.Start(); err != nil {
-            Failed(err, "Start failed.")
-        }
-
-
+    if cmds == nil || len(cmds) == 0 {
+        return nil, errors.New("The cmd slice is invalid.")
     }
 
-    return nil
+    first := true
+    var output []byte
+    var err error
+
+    for _, cmd := range cmds {
+        fmt.Printf("Run Command : %v ...\n", getCmdPlaintext(cmd))
+        if !first {
+            var stdinBuf bytes.Buffer
+            cmd.Stdin = &stdinBuf
+            stdinBuf.Write(output)
+        }
+
+        var stdoutBuf bytes.Buffer
+        cmd.Stdout = &stdoutBuf
+
+        if err = cmd.Start(); err != nil {
+            return nil, getError(err, cmd)
+        }
+
+        if err = cmd.Wait(); err != nil {
+            return nil, getError(err, cmd)
+        }
+
+        output = stdoutBuf.Bytes()
+
+        if first {
+            first = false
+        }
+    }
+
+    lines := make([]string, 0)
+    var outputBuf bytes.Buffer
+    outputBuf.Write(output)
+
+    for {
+        line, err := outputBuf.ReadBytes('\n')
+        if err != nil {
+            if err == io.EOF {
+                break
+            } else {
+                return nil, getError(err, nil)
+            }
+        }
+
+        lines = append(lines, string(line))
+    }
+
+    return lines, nil
+}
+
+func getPids(strs []string) ([]int, error) {
+    pids := make([]int, 0)
+    for _, str := range strs {
+        pid, err := strconv.Atoi(strings.TrimSpace(str))
+        if err != nil {
+            return nil, err
+        }
+        pids = append(pids, pid)
+    }
+
+    return pids, nil
+}
+
+func getCmdPlaintext(cmd *exec.Cmd) string {
+    var buf bytes.Buffer
+    buf.WriteString(cmd.Path)
+    for _, arg := range cmd.Args[1:] {
+        buf.WriteRune(' ')
+        buf.WriteString(arg)
+    }
+
+    return buf.String()
+}
+
+func getError(err error, cmd *exec.Cmd, extraInfo ...string) error {
+    var errMsg string
+    if cmd != nil {
+        errMsg = fmt.Sprintf("%s [%s %v]", err, (*cmd).Path, (*cmd).Args)
+    } else {
+        errMsg = fmt.Sprintf("%s", err)
+    }
+
+    if len(extraInfo) > 0 {
+        errMsg = fmt.Sprintf("%s (%v)", errMsg, extraInfo)
+    }
+
+    return errors.New(errMsg)
 }
 
 func Failed(err error, msg error) {
     fmt.Printf("error: %s[%s]", msg, err)
     return
 }
+
